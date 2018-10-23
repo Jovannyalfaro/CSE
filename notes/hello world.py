251eaf45646bfcf9e43686d67111218835424dcf
Jovanny <42385866+Jovannyalfaro@users.noreply.github.com>
2018-10-23 09:46:48 -0700

    10.23

--- notes/hello world.py
+++ notes/hello world.py
@@ -1,1 +1,28 @@
 print ("hello world!!")
+
+# this is a comment. This has no effect on the cod
+# but this does allow me to do things. I can:
+# 1. Make notes to myself
+# 2. comment pieces of code that do not work
+# 3. Make my code easier to read
+
+print ("look at what happens here. is there any space?")
+print ()
+print()
+print("there should be a couple blank lines here.")
+
+# math
+print(3 + 5)
+print(5 - 2)
+print(3 * 4)
+print(6 / 2)
+
+print("figure this out...")
+print(6 // 2)
+print(5 // 2)
+print(9 // 4)
+
+print("here is another one...")
+print(6 % 2)
+print(5 % 2)
+print(11 % 4)  # modulus (remainder)
