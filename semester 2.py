911556d08126b661f1b71783e4d7c2fcffe65a03
jovanny <mvai@wetigers.org>
2019-01-08 09:44:49 -0800

    semester 2

---
+++ semester 2.py
@@ -0,0 +1,23 @@
+print("hello world")
+
+# This is cool
+
+cars = 5
+driving = True
+
+print("Ihavecars) + "cars")
+
+age = input("How old are you?")
+print(age + "??? you really shouldn't lie about your age.")
+
+colors = ["blue", "orange", "yellow", "green", "cyan"]
+colors.append("cyan")
+
+print(colors)
+colors.pop(0)
+print(colors)
+print(colors[2])
+
+print
+
+
