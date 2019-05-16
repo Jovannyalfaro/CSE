8ddc8301d19c61414bd80a6b48225a865d0a0fab
Jovannyalfaro <mvai@wetigers.org>
2019-05-16 09:47:38 -0700

    sales

--- sales.py
+++ sales.py
@@ -2,11 +2,63 @@ import csv
 
 with open("Book1.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
-    fruits = 13
+    fruits = 0
     clothes = 0
     meat = 0
     beverages = 0
-
-
-
-
+    office_supplies = 0
+    cosmetics = 0
+    snacks = 0
+    personal_care = 0
+    house_hold = 0
+    vegetables = 0
+    baby_food = 0
+    cereal = 0
+    print("processing.....")
+    for row in reader:
+        if row[2] == "Cosmetics":
+            profits = float(row[13])
+            for i in row[13]:
+                cosmetics += 1
+ print("processing.....")
+    for row in reader:
+        if row[2] == "Cosmetics":
+            profits = float(row[13])
+            for i in row[13]:
+                cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
+print("processing.....")
+for row in reader:
+    if row[2] == "Cosmetics":
+        profits = float(row[13])
+        for i in row[13]:
+            cosmetics += 1
