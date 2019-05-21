b2d74b66fad6e30117f80e0b1e4d56209706d358
Jovannyalfaro <mvai@wetigers.org>
2019-05-21 09:43:36 -0700

    new+sales

--- sales.py
+++ sales.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("Book1.csv", 'r') as old_csv:
+with open("Sales Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     fruits = 0
     clothes = 0
@@ -20,45 +20,80 @@ with open("Book1.csv", 'r') as old_csv:
             profits = float(row[13])
             for i in row[13]:
                 cosmetics += 1
- print("processing.....")
     for row in reader:
-        if row[2] == "Cosmetics":
+        if row[2] == "fruits":
             profits = float(row[13])
             for i in row[13]:
-                cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
-print("processing.....")
-for row in reader:
-    if row[2] == "Cosmetics":
-        profits = float(row[13])
-        for i in row[13]:
-            cosmetics += 1
+                fruits += 1
+    for row in reader:
+        if row[2] == "clothes":
+            profits = float(row[13])
+            for i in row[13]:
+                clothes += 1
+    for row in reader:
+        if row[2] == "beverages":
+            profits = float(row[13])
+            for i in row[13]:
+                beverages += 1
+    for row in reader:
+        if row[2] == "office_supplies":
+            profits = float(row[13])
+            for i in row[13]:
+                office_supplies += 1
+    for row in reader:
+        if row[2] == "snacks":
+            profits = float(row[13])
+            for i in row[13]:
+                snacks += 1
+    for row in reader:
+        if row[2] == "personal_care":
+            profits = float(row[13])
+            for i in row[13]:
+                personal_care += 1
+    for row in reader:
+        if row[2] == "house_hold":
+            profits = float(row[13])
+            for i in row[13]:
+                house_hold += 1
+    for row in reader:
+        if row[2] == "house_hold":
+            profits = float(row[13])
+            for i in row[13]:
+                house_hold += 1
+    for row in reader:
+        if row[2] == "house_hold":
+            profits = float(row[13])
+            for i in row[13]:
+                house_hold += 1
+    for row in reader:
+        if row[2] == "house_hold":
+            profits = float(row[13])
+            for i in row[13]:
+                house_hold += 1
+    for row in reader:
+        if row[2] == "vegetables":
+            profits = float(row[13])
+            for i in row[13]:
+                vegetables += 1
+    for row in reader:
+        if row[2] == "baby_food":
+            profits = float(row[13])
+            for i in row[13]:
+                baby_food += 1
+    for row in reader:
+        if row[2] == "cereal":
+            profits = float(row[13])
+            for i in row[13]:
+                cereal += 1
+print("The profits for fruits is %d" % fruits)
+print("The profits for vegetables is %d" % vegetables)
+print("The profits for cereal is %d" % cereal)
+print("The profits for house_hold is %d" % house_hold)
+print("The profits for baby_food is %d" % baby_food)
+print("The profits for beverages is %d" % beverages)
+print("The profits for personal_care is %d" % )
+print("The profits for  is %d" % )
+print("The profits for  is %d" % )
+print("The profits for  is %d" % )
+print("The profits for  is %d" % )
+print("The profits for  is %d" % )
